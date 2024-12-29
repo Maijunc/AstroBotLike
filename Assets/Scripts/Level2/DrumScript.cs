5f94e2b4eecb4e7f228b106ac0f01d3252f7589e
rongjie <shengrongjie666@163.com>
2024-12-29 22:44:40 +0800

    change level2 scene

--- Assets/Scripts/Level2/DrumScript.cs
+++ Assets/Scripts/Level2/DrumScript.cs
@@ -3,23 +3,11 @@ using UnityEngine;
 
 public class DrumScript : MonoBehaviour
 {
-    public float rotationSpeed = 90f;// 旋转速度，单位：度/秒
-    public float rotationX = 90f;  
-    public float rotationY = 90f;
-    public float rotationZ = 90f;
-
-    void Start()
+    public float rotationSpeed = 90f;  // 旋转速度，单位：度/秒
+    [SerializeField] private Vector3 rotationAxis = Vector3.up;
+    void Update()
     {
-        // 持续绕 Y 轴旋转
-        RotateObject();
-    }
-    void RotateObject()
-    {
-        // 旋转一圈需要的时间 = 360 / 旋转速度（度每秒）
-        float rotationDuration = 360f / rotationSpeed;
-
-        // 使用 DoTween 来持续旋转
-        transform.DORotate(new Vector3(rotationX, rotationY, rotationZ), rotationDuration, RotateMode.FastBeyond360)
-                 .SetLoops(-1, LoopType.Incremental);
+        // 每一帧旋转一定的角度
+        transform.Rotate(Vector3.up * rotationSpeed * Time.deltaTime);
     }
 }
